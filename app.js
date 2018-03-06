39f8f1cdf7711945225c49d9a64f133b64802bd5
Jeff Wong <jeffmjwong@gmail.com>
2018-03-06 21:20:49 +1000

    textContent and innerHTML

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+const input = document.querySelector('input.description');
+const p = document.querySelector('p.description');
+const button = document.querySelector('button.description');
 
+button.addEventListener('click', () => {
+  const inputValue = input.value;
+  p.innerHTML = `${inputValue}:`;
+  p.title = "List description"
+});
