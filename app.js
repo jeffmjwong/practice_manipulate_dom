cdee087cf99ee5c2dfffeaf51961e9aa15a7a833
Jeff Wong <jeffmjwong@gmail.com>
2018-03-08 19:43:10 +1000

    Append and remove child elements

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ const listDiv = document.querySelector('.list');
 const descriptionInput = document.querySelector('input.description');
 const descriptionP = document.querySelector('p.description');
 const descriptionButton = document.querySelector('button.description');
-const ul = document.querySelector('ul');
 const addItemInput = document.querySelector('.addItemInput');
 const addItemButton = document.querySelector('.addItemButton');
+const removeItemButton = document.querySelector('.removeItemButton');
 
 descriptionButton.addEventListener('click', () => {
   const inputValue = descriptionInput.value;
   descriptionP.innerHTML = `${inputValue}:`;
   descriptionP.title = "List description";
+  descriptionInput.value = "";
 });
 
 toggleList.addEventListener('click', evt => {
@@ -25,7 +26,15 @@ toggleList.addEventListener('click', evt => {
 
 addItemButton.addEventListener('click', () => {
   const inputItem = addItemInput.value;
+  const ul = document.getElementsByTagName('ul')[0];
   let li = document.createElement('li');
   li.textContent = inputItem;
   ul.appendChild(li);
+  addItemInput.value = "";
+});
+
+removeItemButton.addEventListener('click', () => {
+  const ul = document.getElementsByTagName('ul')[0];
+  const li = document.querySelector('li:last-child');
+  ul.removeChild(li);
 });
